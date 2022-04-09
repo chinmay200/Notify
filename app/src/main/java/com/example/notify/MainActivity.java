5ac51f013af50a104f8230d445b3c8c3b15a5f94
Chinmay Borah <chinmay205765@keshav.du.ac.in>
2022-04-10 02:20:44 +0530

    Adding dialog box:

--- app/src/main/java/com/example/notify/MainActivity.java
+++ app/src/main/java/com/example/notify/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.notify;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView addTask;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        addTask = findViewById(R.id.imageView);
+        addTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Toast.makeText(MainActivity.this, "Added a new task", Toast.LENGTH_SHORT).show();
+                newTask fragment = new newTask();
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.framelayout , fragment);
+                fragmentTransaction.commit();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/notify/newTask.java
@@ -0,0 +1,32 @@
+package com.example.notify;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class newTask extends Fragment {
+
+    View view;
+    Button save;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        save = getView().findViewById(R.id.save);
+        
+//        save.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(getActivity(), "Task added succesfully", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+        view = inflater.inflate(R.layout.fragment_new_task, container, false);
+        return view;
+    }
+}
